15b0ee177508afce23c9478115dbaa164f204b69
AnkitBisain <league@iMac-2.attlocal.net>
2019-03-25 18:29:59 -0700

    GetCalculator

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,16 +15,22 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		String Nickles = JOptionPane.showInputDialog("How many nickles do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+		int nick = Integer.parseInt(Nickles);
+		nick = nick *5;
 		// Ask the user how many dimes they have, and convert their answer
-
+		String Dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+		int dime = Integer.parseInt(Dimes);
+		dime = dime *10;
 		// Ask the user how many quarters they have, and convert their answer
-
+		String Quarters = JOptionPane.showInputDialog("How many quarters do you have?");
+		int quart = Integer.parseInt(Quarters);
+		quart = quart * 25;
 		// Calculate how much money the user has and save it in a double variable 
-
+		double change = dime + nick + quart;
 		// Tell the user how much money they have
+		JOptionPane.showMessageDialog(null, "You have "+change+" cents");
 
 	}
 }
